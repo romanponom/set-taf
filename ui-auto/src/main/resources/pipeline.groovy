222b6e60e17abb3f0e05ba9a14bd6a7c27d50dbe
Roman_Ponomarenko1 <Roman_Ponomarenko1@epam.com>
2020-10-30 12:05:23 +0200

    comment docker agent part

--- ui-auto/src/main/resources/pipeline.groovy
+++ ui-auto/src/main/resources/pipeline.groovy
@@ -9,11 +9,11 @@ pipeline {
             }
         }
     }
-    agent {
+    /*agent {
         docker {
             image 'aerokube/selenoid:latest-release'
             args '-d --name selenoid -p 4444:4444 -v //var/run/docker.sock:/var/run/docker.sock -v ${WORKSPACE}:/etc/selenoid:ro'
         }
     }
-
+*/
 }
