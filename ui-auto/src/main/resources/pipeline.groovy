8ed341a93d28bed400a3720434035beec712c19c
Roman_Ponomarenko1 <Roman_Ponomarenko1@epam.com>
2020-10-30 12:13:22 +0200

    extend pipeline

--- ui-auto/src/main/resources/pipeline.groovy
+++ ui-auto/src/main/resources/pipeline.groovy
@@ -1,19 +1,21 @@
 pipeline {
-    agent {
-        label 'master'
+    agent none
+
+    stage('Checkout') {
+        agent {label 'master'}
+        steps {
+            checkout scm
+        }
     }
-    stages {
-        stage('Checkout') {
-            steps {
-                checkout scm
+    stage('Build image') {
+        agent {
+            docker {
+                image 'aerokube/selenoid:latest-release'
+                args '-d --name selenoid -p 4444:4444 -v //var/run/docker.sock:/var/run/docker.sock -v ${WORKSPACE}/ui-auto/src/main/resources/selenoid/config:/etc/selenoid:ro'
             }
         }
-    }
-    /*agent {
-        docker {
-            image 'aerokube/selenoid:latest-release'
-            args '-d --name selenoid -p 4444:4444 -v //var/run/docker.sock:/var/run/docker.sock -v ${WORKSPACE}:/etc/selenoid:ro'
+        steps {
+            sh 'gradle clean test'
         }
     }
-*/
 }
