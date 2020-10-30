3e410818afc6d7646b2c8a2216fb70589ebda99e
Roman_Ponomarenko1 <Roman_Ponomarenko1@epam.com>
2020-10-30 12:14:19 +0200

    add stages to pipeline

--- ui-auto/src/main/resources/pipeline.groovy
+++ ui-auto/src/main/resources/pipeline.groovy
@@ -1,21 +1,22 @@
 pipeline {
     agent none
-
-    stage('Checkout') {
-        agent {label 'master'}
-        steps {
-            checkout scm
-        }
-    }
-    stage('Build image') {
-        agent {
-            docker {
-                image 'aerokube/selenoid:latest-release'
-                args '-d --name selenoid -p 4444:4444 -v //var/run/docker.sock:/var/run/docker.sock -v ${WORKSPACE}/ui-auto/src/main/resources/selenoid/config:/etc/selenoid:ro'
+    stages {
+        stage('Checkout') {
+            agent { label 'master' }
+            steps {
+                checkout scm
             }
         }
-        steps {
-            sh 'gradle clean test'
+        stage('Build image') {
+            agent {
+                docker {
+                    image 'aerokube/selenoid:latest-release'
+                    args '-d --name selenoid -p 4444:4444 -v //var/run/docker.sock:/var/run/docker.sock -v ${WORKSPACE}/ui-auto/src/main/resources/selenoid/config:/etc/selenoid:ro'
+                }
+            }
+            steps {
+                sh 'gradle clean test'
+            }
         }
     }
 }
